db32ac318f512d4a42cb995fa8a32f952f1b971d
Catwallon <catwallon@proton.me>
2024-10-01 20:54:13 +0200

    + | Now display an empty map

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,45 @@
 #include <SFML/Graphics.hpp>
 
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+#define TILE_WIDTH 128.0f
+#define TILE_HEIGHT 48.0f
+
+std::pair<float, float> toIsometric(float x, float y) {
+    float isoX = (x - y) * (TILE_WIDTH / 2);
+    float isoY = (x + y) * (TILE_HEIGHT / 2);
+    return std::make_pair(isoX, isoY);
+}
+
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(800, 600), "game");
-	while (window.isOpen())
+    sf::RenderWindow window(sf::VideoMode(WINDOW_HEIGHT, WINDOW_WIDTH), "game");
+    sf::Texture texture;
+    if (!texture.loadFromFile("assets/grass.png"))
+        return EXIT_FAILURE;
+    sf::Sprite sprite(texture);
+    sprite.setScale(TILE_WIDTH / texture.getSize().x, TILE_HEIGHT / texture.getSize().y);
+    sprite.setOrigin(TILE_WIDTH * 2, 0);
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear();
+	for (int y = 0; y <= 10; y++)
 	{
-		sf::Event event;
-		while (window.pollEvent(event))
+		for (int x = 0; x <= 10; x++)
 		{
-			if (event.type == sf::Event::Closed)
-				window.close();
+			std::pair<float, float> isoPos = toIsometric(x, y);
+			sprite.setPosition(isoPos.first + WINDOW_HEIGHT / 2, isoPos.second);
+        		window.draw(sprite);
 		}
-		window.display();
 	}
-	return EXIT_SUCCESS;
+        window.display();
+    }
+    return EXIT_SUCCESS;
 }
+
