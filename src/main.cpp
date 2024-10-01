b4e15b678505e2281511992fcdf8e322fb816511
Catwallon <catwallon@proton.me>
2024-10-01 23:11:11 +0200

    ~ | Small fixes

--- src/main.cpp
+++ src/main.cpp
@@ -5,41 +5,41 @@
 #define TILE_WIDTH 128.0f
 #define TILE_HEIGHT 48.0f
 
-std::pair<float, float> toIsometric(float x, float y) {
-    float isoX = (x - y) * (TILE_WIDTH / 2);
-    float isoY = (x + y) * (TILE_HEIGHT / 2);
-    return std::make_pair(isoX, isoY);
+sf::Vector2f toIsometric(float x, float y)
+{
+	float isoX = (x - y) * (TILE_WIDTH / 2);
+	float isoY = (x + y) * (TILE_HEIGHT / 2);
+	return sf::Vector2f(isoX, isoY);
 }
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(WINDOW_HEIGHT, WINDOW_WIDTH), "game");
-    sf::Texture texture;
-    if (!texture.loadFromFile("assets/grass.png"))
-        return EXIT_FAILURE;
-    sf::Sprite sprite(texture);
-    sprite.setScale(TILE_WIDTH / texture.getSize().x, TILE_HEIGHT / texture.getSize().y);
-    sprite.setOrigin(TILE_WIDTH * 2, 0);
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-        window.clear();
-	for (int y = 0; y <= 10; y++)
+	sf::RenderWindow window(sf::VideoMode(WINDOW_HEIGHT, WINDOW_WIDTH), "game");
+	sf::Texture texture;
+	if (!texture.loadFromFile("assets/grass.png"))
+		return EXIT_FAILURE;
+	sf::Sprite sprite(texture);
+	sprite.setScale(TILE_WIDTH / texture.getSize().x, TILE_HEIGHT / texture.getSize().y);
+	sprite.setOrigin(texture.getSize().x / 2, 0);
+	while (window.isOpen())
 	{
-		for (int x = 0; x <= 10; x++)
+		sf::Event event;
+		while (window.pollEvent(event))
 		{
-			std::pair<float, float> isoPos = toIsometric(x, y);
-			sprite.setPosition(isoPos.first + WINDOW_HEIGHT / 2, isoPos.second);
-        		window.draw(sprite);
+			if (event.type == sf::Event::Closed)
+				window.close();
+        }
+		window.clear();
+		for (int y = 0; y <= 10; y++)
+		{
+			for (int x = 0; x <= 10; x++)
+			{
+				auto isoPos = toIsometric(x, y);
+				sprite.setPosition(isoPos.x + WINDOW_HEIGHT / 2, isoPos.y);
+				window.draw(sprite);
+			}
 		}
+		window.display();
 	}
-        window.display();
-    }
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
-
